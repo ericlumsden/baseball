89628e50b96dcc2b3373ddfce730f69ea5178d3c
ericwlumsden <airique21@gmail.com>
2021-06-28 09:08:26 -0700

    split median and mean for svd

--- grouping_hitters/hitters_svd.py
+++ grouping_hitters/hitters_svd.py
@@ -10,16 +10,20 @@ df_statcast = pd.read_csv('statcast_clean_TRUNCATED.csv', index_col=0)
 all_list = ['Name', 'R', '2B', '3B', 'HR', 'BB', 'IBB', 'SO', 'BA', 'OBP', 'SLG', 'OPS', 'bbType_flyBall', 'bbType_groundBall', 'bbType_lineDrive', 'bbType_popUp', '2BperPA', '3BperPA', 'HRperPA', 'BBperPA', 'IBBperPA', 'SOperPA']
 df_all = df_all[all_list]
 
-statcast_list = all_list + ['launch_speed_median','launch_speed_mean','launch_angle_median','launch_angle_mean']
-df_trunc = df_statcast[statcast_list]
+statcast_list_median = all_list + ['launch_speed_median','launch_angle_median']
+statcast_list_mean = all_list + ['launch_speed_mean','launch_angle_mean']
+df_trunc_median = df_statcast[statcast_list_median]
+df_trunc_mean = df_statcast[statcast_list_mean]
 
 # Then convert those selected dataframes to arrays
 all_array = df_all.to_numpy()
-statcast_array = df_trunc.to_numpy()
+statcast_array_median = df_trunc_median.to_numpy()
+statcast_array_mean = df_trunc_mean.to_numpy()
 
 # Then run the SVDs! Just on the columns 12+ (bbType_flyBall onward)
 u_all, sig_all, v_all = np.linalg.svd(all_array[:, 12:].astype(float))
-u_trunc, sig_trunc, v_trunc = np.linalg.svd(statcast_array[:, 12:].astype(float))
+u_median, sig_median, v_median = np.linalg.svd(statcast_array_median[:, 12:].astype(float))
+u_mean, sig_mean, v_mean = np.linalg.svd(statcast_array_mean[:, 12:].astype(float))
 
 # Now, will make lists of the sigma values for the two approaches to graph and compare
 s_all_running_count = 0
@@ -28,15 +32,22 @@ for s in sig_all:
     s_all_running_count += s
     s_all_list.append(s_all_running_count / np.sum(sig_all))
 
-s_trunc_running_count = 0
-s_trunc_list = []
-for s in sig_trunc:
+s_trunc_median_count = 0
+s_trunc_median_list = []
+for s in sig_median:
     s_trunc_running_count += s
-    s_trunc_list.append(s_trunc_running_count / np.sum(sig_trunc))
+    s_trunc_median_list.append(s_trunc_median_count / np.sum(sig_median))
+
+s_trunc_mean_count = 0
+s_trunc_mean_list = []
+for s in sig_mean:
+    s_trunc_running_count += s
+    s_trunc_mean_list.append(s_trunc_mean_count / np.sum(sig_mean))
 
 plt.figure(1)
-plt.plot(range(len(s_all_list)), s_all_list, c='k', label='All Players')
-plt.plot(range(len(s_trunc_list)), s_trunc_list, c='r', label='Truncated Players')
+plt.plot(range(len(s_all_list)), s_all_list, c='r', label='All Players')
+plt.plot(range(len(s_trunc_median_list)), s_trunc_median_list, c='k', label='Truncated Players [median]')
+plt.plot(range(len(s_trunc_mean_list)), s_trunc_mean_list, c='b', label='Truncated Players [mean]')
 plt.xlabel('s#')
 plt.ylabel('% variance explained')
 plt.title('comparing % variance')
@@ -47,12 +58,15 @@ V_r_all = v_all[:,:2]
 V_r_trunc = v_trunc[:,:2]
 
 a_all = np.matmul(all_array[:,12:].astype(float), V_r_all)
-a_trunc = np.matmul(statcast_array[:,12:].astype(float), V_r_trunc)
+a_median = np.matmul(statcast_array[:,12:].astype(float), V_r_median)
+a_mean = np.matmul(statcast_array[:,12:].astype(float), V_r_mean)
 
 plt.figure(2)
 plt.plot(a_all[:,0], a_all[:,1], '.k')
 
 plt.figure(3)
-plt.plot(a_trunc[:,0], a_trunc[:,1], '.k')
+plt.plot(a_median[:,0], a_median[:,1], '.k')
 
+plt.figure(4)
+plt.plot(a_mean[:,0], a_mean[:,1], '.k')
 plt.show()
